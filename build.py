2704c2c4bfd52c61c70638e2d16b1b4c9c6747d6
sanketp79 <palsulesanket@gmail.com>
2017-05-17 10:26:56 +0530

    Add files via upload

---
+++ build.py
@@ -0,0 +1,12 @@
+from itertools import chain, combinations
+
+def powerset(iterable):
+
+    xs = list(iterable)
+    # note we return an iterator rather than a list
+    return chain.from_iterable(combinations(xs,n) for n in range(len(xs)+1))
+
+
+
+# what is input? (2,3,4)
+# output should be the output? () (2) (3 ) (4) (2,3) (2,4) (3,4) (2,3,4)
